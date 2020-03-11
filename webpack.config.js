6eb7ce53af3cc22f3c70461115fd231047a62a47
M.Fredd <sk3250@mail.ru>
2020-03-11 11:40:08 +0500

    .nowignore

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ['babel-loader', 'eslint-loader'],
+        use: isDev ? ['babel-loader', 'eslint-loader'] : 'babel-loader',
       },
       {
         test: /\.css$/,
